077da30171dedc27a97319ce935f58ad603a0af3
Cerlia <94546733+Cerlia@users.noreply.github.com>
2023-06-04 07:59:13 +0200

    Delete site directory

--- site/controllers/c_BookingController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class BookingController {
-    public function addBooking($id_lesson, Code $id_code){
-
-        $bookingPDO = new bookingPDO();
-        $bookingPDO->create($id_lesson,$id_code);
-    }
-}
-?>
